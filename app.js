87b7a619db2ac92669040f7a82bb7ae0fced7d6e
KenWong <wongkenq@gmail.com>
2022-09-10 18:16:17 -0700

    completed basic timer function

--- app.js
+++ app.js
@@ -1,65 +1,85 @@
-const timer = {
+let timerName = {
     randori: 0,
     uchikomi: 0,
     break: 0,
     waterBreak: 0,
 }
 
-let interval
-
-const addTimeBtn = document.querySelector('.fa-plus')
-const subTimeBtn = document.querySelector('.fa-minus')
-const displayMin = document.getElementById('js-minutes')
-const displaySec = document.getElementById('js-seconds')
-const startBtn = document.getElementById('js-btn')
+let currentTime, endTime, differenceTime, remainingTime
 
 const adjustTime = {
     add: () => {
-        timer.randori++
-        displayMin.textContent = `${timer.randori}`.padStart(2, '0')
+        timerName.randori++
+        displayMin.textContent = `${timerName.randori}`.padStart(2, '0')
     },
     subtract: () => {
-        if (timer.randori > 0){
-            timer.randori--
-            displayMin.textContent = `${timer.randori}`.padStart(2, '0')
+        if (timerName.randori > 0){
+            timerName.randori--
+            displayMin.textContent = `${timerName.randori}`.padStart(2, '0')
         } else {
             return
         }        
     }
 }
 
+let interval
+const addTimeBtn = document.querySelector('.fa-plus')
+const subTimeBtn = document.querySelector('.fa-minus')
 addTimeBtn.addEventListener('click', adjustTime.add)
 subTimeBtn.addEventListener('click', adjustTime.subtract)
-
-const getRemainingTime = (endTime) => {
-    const currentTime = Date.parse(new Date())
-    const difference = endTime - currentTime
-
-    console.log(difference)
-
-    const minutes = Math.floor(difference / 60 / 1000)
-    const seconds = Math.floor(difference / 1000) % 60
-    
-    return {
-        difference,
-        minutes,
-        seconds,
-    }
-} 
-
-const startTimer = () => {
-    let minutes = +displayMin.textContent * 60
-    let seconds = +displaySec.textContent
-    let endTime = ((minutes + seconds) * 1000) + Date.parse(new Date())
-    
-    getRemainingTime(endTime)
-}
-
+const displayMin = document.getElementById('js-minutes')
+const displaySec = document.getElementById('js-seconds')
+const startBtn = document.getElementById('js-btn')
 startBtn.addEventListener('click', startTimer)
-
 const fullscreenBtn = document.getElementById('fullscreen')
+
 fullscreenBtn.addEventListener('click', () => {
     document.documentElement.requestFullscreen().catch((e) => {
         console.log(e)
     })
 })
+
+function getRemainingTime(endTime) {
+    currentTime = Date.parse(new Date())
+    differenceTime = +(endTime - currentTime)
+
+    console.log('times.difference: ' + differenceTime)
+
+    const minutes = Math.floor(differenceTime / 60 / 1000)
+    const seconds = Math.floor(differenceTime / 1000) % 60
+
+    return {
+        // difference,
+        minutes,
+        seconds,
+    }
+} 
+
+function startTimer() {
+    let minutes = +displayMin.textContent * 60
+    let seconds = +displaySec.textContent
+    let endTime = ((minutes + seconds) * 1000) + Date.parse(new Date())
+    console.log('minutes: ' + minutes, 'seconds: ' + seconds)
+    console.log('endtime: ' + endTime)
+    getRemainingTime(endTime)
+    
+    interval = setInterval(() => {
+        remainingTime = getRemainingTime(endTime)
+        updateClock()
+
+        if(differenceTime <= 0){
+            clearInterval(interval)
+        }
+    }, 1000);
+}
+
+function updateClock() {
+    const minutes = Math.floor(differenceTime / 60 / 1000)
+    const seconds = Math.floor(differenceTime / 1000) % 60
+    // console.log(differenceTime)
+    // console.log(minutes)
+    // console.log(seconds)
+
+    displayMin.textContent = `${minutes}`.padStart(2, '0')
+    displaySec.textContent = `${seconds}`.padStart(2, '0')
+}
