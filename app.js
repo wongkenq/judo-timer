ceb6dd532a06db83e69faa7432b9dba277ea56aa
KenWong <wongkenq@gmail.com>
2022-10-04 23:34:08 -0700

    added get resolution and scroll to element functions

--- app.js
+++ app.js
@@ -1,22 +1,45 @@
 let timerName = {
-    randori: {
-        minutes: 0,
-        seconds: 0,
-        rounds: 0,
-    },
-    uchikomi: {
-        minutes: 0,
-        seconds: 0,
-    },
-    waterBreak: {
-        minutes: 2,
-        seconds: 0,
-    },
-    break: {
-        minutes: 0,
-        seconds: 30,
-    },
+  randori: {
+    minutes: 0,
+    seconds: 2,
+    rounds: 2,
+  },
+  uchikomi: {
+    minutes: 0,
+    seconds: 0,
+  },
+  waterBreak: {
+    minutes: 2,
+    seconds: 0,
+  },
+  break: {
+    minutes: 0,
+    seconds: 3,
+  },
 }
+
+let screenResolution = {
+  width: 0,
+  height: 0,
+}
+
+function getScreenResolution() {
+  screenResolution.width = window.innerWidth
+  screenResolution.height = window.innerHeight
+
+  document.querySelector('body').style.height = `${screenResolution.height}px`
+
+  console.log(screenResolution)
+
+  const main = document.querySelectorAll('main')
+  main.forEach((x) => (x.style.height = `${screenResolution.height}px`))
+  main.forEach((x) => (x.style.width = `${screenResolution.width}px`))
+
+  console.log(main[0].style.height)
+}
+
+window.onload = getScreenResolution
+window.addEventListener('resize', getScreenResolution)
 
 let currentTime, endTime, differenceTime, remainingTime
 let currentMode = 'randori'
@@ -24,20 +47,32 @@ let masterMode
 let currentRound = 1
 
 const adjustTime = {
-    add: () => {
-        timerName[currentMode]['minutes']++
-        displayMin.textContent = `${timerName[currentMode]['minutes']}`.padStart(2, '0')
-        displaySec.textContent = `${timerName[currentMode]['seconds']}`.padStart(2, '0')
-    },
-    subtract: () => {
-        if (timerName[currentMode]['minutes'] > 0){
-            timerName[currentMode]['minutes']--
-            displayMin.textContent = `${timerName[currentMode]['minutes']}`.padStart(2, '0')
-            displaySec.textContent = `${timerName[currentMode]['seconds']}`.padStart(2, '0')
-        } else {
-            return
-        }        
+  add: () => {
+    timerName[currentMode]['minutes']++
+    displayMin.textContent = `${timerName[currentMode]['minutes']}`.padStart(
+      2,
+      '0'
+    )
+    displaySec.textContent = `${timerName[currentMode]['seconds']}`.padStart(
+      2,
+      '0'
+    )
+  },
+  subtract: () => {
+    if (timerName[currentMode]['minutes'] > 0) {
+      timerName[currentMode]['minutes']--
+      displayMin.textContent = `${timerName[currentMode]['minutes']}`.padStart(
+        2,
+        '0'
+      )
+      displaySec.textContent = `${timerName[currentMode]['seconds']}`.padStart(
+        2,
+        '0'
+      )
+    } else {
+      return
     }
+  },
 }
 
 let interval
@@ -49,180 +84,203 @@ const displaySec = document.getElementById('js-seconds')
 const startBtn = document.getElementById('js-btn')
 const resetBtn = document.getElementById('js-reset-btn')
 const fullscreenBtn = document.getElementById('fullscreen')
-const progress = document.getElementById("js-progress")
-const buttonSound = new Audio("button-sound.mp3");
+const progress = document.getElementById('js-progress')
+// const buttonSound = new Audio('button-sound.mp3')
 const roundSelect = document.getElementById('round-select')
 
-addTimeBtn.addEventListener('click', adjustTime.add)
-subTimeBtn.addEventListener('click', adjustTime.subtract)
+// addTimeBtn.addEventListener('click', adjustTime.add)
+// subTimeBtn.addEventListener('click', adjustTime.subtract)
 resetBtn.addEventListener('click', resetTimer)
 resetBtn.addEventListener('mousedown', buttonDown)
 resetBtn.addEventListener('touchstart', buttonDown)
 resetBtn.addEventListener('mouseup', buttonUp)
 resetBtn.addEventListener('touchend', buttonUp)
-fullscreenBtn.addEventListener('click', fullscreenChange)
+// fullscreenBtn.addEventListener('click', fullscreenChange)
 
 function buttonDown(e) {
-    e.target.classList.add('active')
+  e.target.classList.add('active')
 }
 
 function buttonUp(e) {
-    e.target.classList.remove('active')
+  e.target.classList.remove('active')
 }
 
 function fullscreenChange() {
-    if (document.fullscreenElement) {
-        document.exitFullscreen()
-    } else {
-        document.documentElement.requestFullscreen()
-    }
+  if (document.fullscreenElement) {
+    document.exitFullscreen()
+  } else {
+    document.documentElement.requestFullscreen()
+  }
 }
 
 startBtn.addEventListener('click', (e) => {
-    if (e.target.dataset.action === 'start') startTimer()
-    else pauseTimer()
+  if (e.target.dataset.action === 'start') startTimer()
+  else pauseTimer()
 })
 
-roundSelect.addEventListener('click', (e) => {
-    timerName.randori.rounds = +roundSelect.value
-    
-})
+// roundSelect.addEventListener('click', (e) => {
+//   timerName.randori.rounds = +roundSelect.value
+// })
 
 function getRemainingTime(endTime) {
-    currentTime = Date.parse(new Date())
-    differenceTime = +(endTime - currentTime)
+  currentTime = Date.parse(new Date())
+  differenceTime = +(endTime - currentTime)
 
-    const minutes = Math.floor(differenceTime / 60 / 1000)
-    const seconds = Math.floor(differenceTime / 1000) % 60
+  const minutes = Math.floor(differenceTime / 60 / 1000)
+  const seconds = Math.floor(differenceTime / 1000) % 60
 
-    return {
-        // difference,
-        minutes,
-        seconds,
-    }
-} 
+  return {
+    // difference,
+    minutes,
+    seconds,
+  }
+}
 
 function startTimer() {
-    let minutes = +timerName[currentMode]['minutes'] * 60
-    let seconds = +timerName[currentMode]['seconds']
+  let minutes = +timerName[currentMode]['minutes'] * 60
+  let seconds = +timerName[currentMode]['seconds']
 
-    if (timerName[currentMode].minutes === 0 && timerName[currentMode]['seconds'] === 0) return
+  if (
+    timerName[currentMode].minutes === 0 &&
+    timerName[currentMode]['seconds'] === 0
+  )
+    return
 
-    if (differenceTime > 0) endTime = differenceTime + Date.parse(new Date())
-    else endTime = ((minutes + seconds) * 1000) + Date.parse(new Date())
+  if (differenceTime > 0) endTime = differenceTime + Date.parse(new Date())
+  else endTime = (minutes + seconds) * 1000 + Date.parse(new Date())
 
-    getRemainingTime(endTime)
-    
-    // buttonSound.play()
-    
-    startBtn.dataset.action = 'pause'
-    startBtn.textContent = 'pause'
-    startBtn.classList.add('active')
-    
-    document.getElementById('round-select-output').textContent = currentRound
+  getRemainingTime(endTime)
 
-    interval = setInterval(() => {
-        remainingTime = getRemainingTime(endTime)
-        updateClock()
+  // buttonSound.play()
 
-        if (differenceTime < 0) {
+  startBtn.dataset.action = 'pause'
+  startBtn.textContent = 'pause'
+  startBtn.classList.add('active')
+
+  document.getElementById('round-select-output').textContent = currentRound
+
+  interval = setInterval(() => {
+    remainingTime = getRemainingTime(endTime)
+    updateClock()
+
+    if (differenceTime < 0) {
+      clearInterval(interval)
+      resetTimer()
+
+      switch (currentMode) {
+        case 'randori':
+          if (currentRound < timerName.randori.rounds) {
+            console.log(`current round: ${currentRound}`)
             clearInterval(interval)
-            resetTimer()
-            
-            switch (currentMode) {
-                case 'randori':
-                    if (currentRound < timerName.randori.rounds) {
-                        console.log(`current round: ${currentRound}`)
-                        clearInterval(interval)
-                        switchMode('break')
-                        startTimer()
-                    } else {
-                        clearInterval(interval)
-                        currentRound = 1
-                        document.getElementById('round-select-output').textContent = currentRound
-                    }
-                    break
-                case 'break':
-                    clearInterval(interval)
-                    if (masterMode === 'break') return
-                    else {
-                        currentRound++
-                        document.getElementById('round-select-output').textContent = currentRound
+            switchMode('break')
+            startTimer()
+          } else {
+            clearInterval(interval)
+            currentRound = 1
+            document.getElementById('round-select-output').textContent =
+              currentRound
+          }
+          break
+        case 'break':
+          clearInterval(interval)
+          if (masterMode === 'break') return
+          else {
+            currentRound++
+            document.getElementById('round-select-output').textContent =
+              currentRound
 
-                        switchMode('randori')
-                        startTimer()
-                    }
-                break
-            }
-        }
-    }, 1000);
+            switchMode('randori')
+            startTimer()
+          }
+          break
+      }
+    }
+  }, 1000)
 }
 
 function pauseTimer() {
-    clearInterval(interval)
+  clearInterval(interval)
 
-    startBtn.dataset.action = 'start'
-    startBtn.textContent = 'start'
-    startBtn.classList.remove('active')
+  startBtn.dataset.action = 'start'
+  startBtn.textContent = 'start'
+  startBtn.classList.remove('active')
 }
 
 function updateClock() {
-    const minutes = Math.floor(differenceTime / 60 / 1000)
-    const seconds = Math.floor(differenceTime / 1000) % 60
+  const minutes = Math.floor(differenceTime / 60 / 1000)
+  const seconds = Math.floor(differenceTime / 1000) % 60
 
-    displayMin.textContent = `${minutes}`.padStart(2, '0')
-    displaySec.textContent = `${seconds}`.padStart(2, '0')
+  displayMin.textContent = `${minutes}`.padStart(2, '0')
+  displaySec.textContent = `${seconds}`.padStart(2, '0')
 
-    const total = (timerName[currentMode]['minutes'] * 60) + timerName[currentMode]['seconds']
-    const remaining = remainingTime.minutes * 60 + remainingTime.seconds
-    progress.max = total
-    progress.value = total - remaining
+  const total =
+    timerName[currentMode]['minutes'] * 60 + timerName[currentMode]['seconds']
+  const remaining = remainingTime.minutes * 60 + remainingTime.seconds
+  progress.max = total
+  progress.value = total - remaining
 }
 
 function resetTimer() {
-    pauseTimer()
-    getRemainingTime(Date.parse(new Date()))
+  pauseTimer()
+  getRemainingTime(Date.parse(new Date()))
 
-    // if (currentRound === timerName.randori.rounds) currentRound = 1
+  // if (currentRound === timerName.randori.rounds) currentRound = 1
 
-    // document.getElementById('round-select-output').textContent = currentRound
+  // document.getElementById('round-select-output').textContent = currentRound
 
-    progress.value = 0
-    
-    displayMin.textContent = `${timerName[currentMode]['minutes']}`.padStart(2, '0')
-    displaySec.textContent = `${timerName[currentMode]['seconds']}`.padStart(2, '0')
+  progress.value = 0
+
+  displayMin.textContent = `${timerName[currentMode]['minutes']}`.padStart(
+    2,
+    '0'
+  )
+  displaySec.textContent = `${timerName[currentMode]['seconds']}`.padStart(
+    2,
+    '0'
+  )
 }
 
 const modeBtn = document.getElementById('js-mode-buttons')
 modeBtn.addEventListener('click', handleMode)
 
 function handleMode(event) {
-    const modes = event.target.dataset.mode
+  const modes = event.target.dataset.mode
 
-    if (!modes) return
+  if (!modes) return
 
-    masterMode = modes
+  masterMode = modes
 
-    switchMode(modes)
-    resetTimer()
+  switchMode(modes)
+  resetTimer()
 }
 
-function switchMode(mode){
-    currentMode = mode
-    console.log(currentMode)
+function switchMode(mode) {
+  currentMode = mode
+  console.log(currentMode)
 
-    displayMin.textContent = `${timerName[currentMode]['minutes']}`.padStart(2, '0')
-    displaySec.textContent = `${timerName[currentMode]['seconds']}`.padStart(2, '0')
+  displayMin.textContent = `${timerName[currentMode]['minutes']}`.padStart(
+    2,
+    '0'
+  )
+  displaySec.textContent = `${timerName[currentMode]['seconds']}`.padStart(
+    2,
+    '0'
+  )
 
-    document
-        .querySelectorAll('button[data-mode]')
-        .forEach((e) => e.classList.remove('active'))
-    document.querySelector(`[data-mode='${currentMode}']`).classList.add('active')
-    document.body.style.backgroundColor = `var(--${currentMode})`
+  document
+    .querySelectorAll('button[data-mode]')
+    .forEach((e) => e.classList.remove('active'))
+  document.querySelector(`[data-mode='${currentMode}']`).classList.add('active')
+  document.body.style.backgroundColor = `var(--${currentMode})`
 }
 
 const modeButtons = document.querySelectorAll('.mode-button')
 
-modeButtons.forEach(e => {
-    if(e.classList.contains('active')) console.log(`${e.dataset.mode} is active`)
+modeButtons.forEach((e) => {
+  if (e.classList.contains('active')) console.log(`${e.dataset.mode} is active`)
 })
+
+function scrollToTimer() {
+  let timer = document.querySelector('.app')
+  timer.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' })
+}
